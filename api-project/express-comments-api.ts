fe28ac2c553f7bead0bd4c93e2b51c0814793166
pizarrosch <bayenrshc89@gmail.com>
2023-09-15 00:22:44 +0200

    Install uuid to generate a random id, then overwrite mock-comment.json via writeFile function while concatinating generated id with the rest of the data

--- api-project/express-comments-api.ts
+++ api-project/express-comments-api.ts
@@ -1,26 +1,58 @@
 import express, { Request, Response } from 'express';
-import {IComment} from "./types";
-import {readFile} from "fs/promises";
+import {readFile, writeFile} from "fs/promises";
+import { v4 as uuidv4 } from 'uuid';
+import {CommentCreatePayload} from "./types";
 
 const app = express();
 const jsonMiddleware = express.json();
 app.use(jsonMiddleware);
 
-const PATH = 'api/comments';
+const PATH = '/api/comments';
 
-const loadComments = async (): Promise<IComment[]> => {
+const loadComments = async (): Promise<CommentCreatePayload[]> => {
     const rawData = await readFile("mock-comment.json", "binary");
-    return JSON.parse(rawData.toString());
+    return JSON.parse(rawData);
 }
 
-const server = app.get(PATH, async (req: Request, res: Response) => {
-    const comments = await loadComments();
+const saveComments = async (data: CommentCreatePayload[]): Promise<void> => {
+    await writeFile("mock-comment.json", JSON.stringify(data));
+}
+
+app.get(PATH, async (req: Request, res: Response) => {
+    const comments: CommentCreatePayload[] = await loadComments();
     res.setHeader('Content-Type', 'application/json');
     res.send(comments);
 });
 
-const PORT = 3000;
+const validateComment = (body: CommentCreatePayload | null) => {
+    if (!body) {
+        return 'No body found!';
+    }
+}
 
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.post(PATH, async (req: Request<{}, {}, CommentCreatePayload>, res: Response) => {
+
+    const validationResult = validateComment(req.body);
+
+    if (validationResult) {
+        res.status(400);
+        res.send(validationResult);
+        return;
+    }
+
+    const id = await uuidv4();
+    const idObj: {id: string} = {id: id};
+    const dataObj = req.body;
+    const concatObj = Object.assign({}, idObj, dataObj);
+
+    const comments: CommentCreatePayload[] = await loadComments();
+    comments.push(concatObj);
+    await saveComments(comments)
+
+    res.status(201);
+    res.send(`Comment id:${id} has been added!`);
+});
+
+app.listen(3002, () => {
+    console.log(`Server running on port 3002`);
 });
--- api-project/types.ts
+++ api-project/types.ts
@@ -1,7 +1,11 @@
 export interface IComment {
-    id: number;
+    id: string;
     name: string;
     email: string;
     body: string;
     postId: number;
 }
+
+export type CommentCreatePayload = Omit<IComment, "id">;
+// Omit is a typescript helper, which returns the given type (first parameter in the generic type)
+// and excludes the given key (second parameter)
